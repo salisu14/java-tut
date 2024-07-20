2a12d8e889ca3adf46e5a69bda505b921c69c435
Muhammad S. Ali <salisu14@yahoo.com>
2024-07-20 12:21:48 +0100

    Add .gitignore file.

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HelloGit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Welcome to Git Distributed VCS");
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter your name: ");
+        String name = input.readLine();
+
+        System.out.println("Your name is: " + name);
     }
 }
